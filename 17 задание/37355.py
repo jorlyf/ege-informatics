394f3ad24d80fe0640f4660a62c28719aefd140d
shlyapp <shlyapp.exe@gmail.com>
2021-12-02 00:45:53 +0400

    решение 37355

--- 17 задание/37355.py
+++ 17 задание/37355.py
@@ -1,2 +1,16 @@
-f = open("37355.txt")
-print(f)
+f = open("17 задание/37355.txt")
+data = []
+
+for element in f:
+    data.append(int(element))
+
+counter = 0
+maximum = 0
+
+for i in range(len(data)):
+    for j in range(len(data)):
+        if (data[i] + data[j]) % 7 == 0:
+            counter += 1
+            maximum = max(maximum, data[i] + data[j])
+
+print(counter, maximum)
